ba795d5beb889d635d883a6aca8e267a8a682444
Sicco van Sas <siccovansas@users.noreply.github.com>
2019-07-03 19:49:37 +0200

    Update Fabric to v2

--- fabfile.py
+++ fabfile.py
@@ -1,18 +1,24 @@
-from fabric.api import run, env, cd, sudo
-
-env.use_ssh_config = True
-env.hosts = ["Oxygen"]
+from fabric import Connection, Config, task
+from invoke import Exit
+import getpass
 
 # Name of the git repository
 GIT_REPO = 'open-state-theme-2019'
 
+# Path of the directory
+DIR = '/home/projects/wordpress-new/wp-content/themes/%s' % (GIT_REPO)
+
 # Container used to compile the assets
 NODE_CONTAINER = 'openstatetheme2019_node_1'
 
-def deploy():
-    with cd('/home/projects/wordpress-new/wp-content/themes/%s' % (GIT_REPO)):
-        sudo('git pull git@github.com:openstate/%s.git' % (GIT_REPO))
-        output = sudo('docker inspect --format="{{.State.Status}}" %s' % (NODE_CONTAINER))
-        if output != 'running':
-            exit('\n*** ERROR: The %s container, used to compile the assets, is not running. Please build/run/start the container.' % (NODE_CONTAINER))
-        sudo('docker exec %s yarn build:production' % (NODE_CONTAINER))
+@task
+def deploy(c):
+    sudo_pass = getpass.getpass("What's your sudo password?")
+    config = Config(overrides={'sudo': {'password': sudo_pass}})
+    c = Connection('Oxygen', config=config)
+
+    c.run('cd %s && git pull git@github.com:openstate/%s.git' % (DIR, GIT_REPO))
+    output = c.sudo('docker inspect --format="{{.State.Status}}" %s' % (NODE_CONTAINER))
+    if output.stdout.strip() != 'running':
+        raise Exit('\n*** ERROR: The %s container, used to compile the assets, is not running. Please build/run/start the container.' % (NODE_CONTAINER))
+    c.sudo('docker exec %s yarn build:production' % (NODE_CONTAINER))
