e4ea4813455446a233d9663a2316b2fede82b607
Sicco van Sas <siccovansas@users.noreply.github.com>
2019-07-04 02:19:24 +0200

    Fix ordering on frontpage

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -86,7 +86,7 @@
 
   <div class="container">
     <div class="row">
-      <div class="col-lg-8 order-md-last">
+      <div class="col-lg-8 order-12 order-lg-1">
         <div class="row">
           <div class="col-12 nieuws-agenda">
             <? _e("
@@ -125,7 +125,7 @@
         ?>
         </div>
       </div>
-      <div class="col-lg-4">
+      <div class="col-lg-4 order-1 order-lg-12">
         <div class="row agenda">
           <div class="col-12 nieuws-agenda sidebar text-donkerpaars bg-grijs-10">
             <? _e("
