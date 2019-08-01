a0ca7cbbfde8184fa02dd7a57f6f3efb5e285747
Sicco van Sas <siccovansas@users.noreply.github.com>
2019-08-01 17:08:16 +0200

    Add status meter icons in the controls

--- resources/views/partials/status-carousel-controls.blade.php
+++ resources/views/partials/status-carousel-controls.blade.php
@@ -6,9 +6,20 @@
         <br>
         <br>
       </div>
-      <img class="d-inline d-xl-block img-fluid" src="@asset('images/OSF-Icon-lobby-l.svg')">
-      <div class="d-inline d-xl-none">
-        Lobby
+      <img class="align-top d-inline d-xl-block img-fluid" src="@asset('images/OSF-Icon-lobby-l.svg')">
+      <div class="d-inline-block">
+        <div class="d-inline d-xl-none">
+          Lobby
+        </div>
+        <div class="status-meter-icon-wrapper">
+          @foreach (range(1, 5) as $num)
+            @if (get_field('project_status_lobby_score') == $num)
+              <div class="status-meter-icon status-score-{{ $num }} mx-auto"></div>
+            @else
+              <div class="status-meter-icon mx-auto"></div>
+            @endif
+          @endforeach
+        </div>
       </div>
     </div>
   </li>
@@ -19,9 +30,20 @@
         <br>
         <br>
       </div>
-      <img class="d-inline d-xl-block img-fluid" src="@asset('images/OSF-Icon-data-beschikbaar-l.svg')">
-      <div class="d-inline d-xl-none">
-        Data beschikbaar
+      <img class="align-top d-inline d-xl-block img-fluid" src="@asset('images/OSF-Icon-data-beschikbaar-l.svg')">
+      <div class="d-inline-block">
+        <div class="d-inline d-xl-none">
+          Data beschikbaar
+        </div>
+        <div class="status-meter-icon-wrapper">
+          @foreach (array(1, 2, 3, 4, 5) as $num)
+            @if (get_field('project_status_data_beschikbaar_score') == $num)
+              <div class="status-meter-icon status-score-{{ $num }} mx-auto"></div>
+            @else
+              <div class="status-meter-icon mx-auto"></div>
+            @endif
+          @endforeach
+        </div>
       </div>
     </div>
   </li>
@@ -30,9 +52,20 @@
       <div class="d-none d-xl-block">
         Ontsluiten en hulpmiddelen
       </div>
-      <img class="d-inline d-xl-block img-fluid" src="@asset('images/OSF-Icon-hulpmiddelen-l.svg')">
-      <div class="d-inline d-xl-none">
-        Ontsluiten en hulpmiddelen
+      <img class="align-top d-inline d-xl-block img-fluid" src="@asset('images/OSF-Icon-hulpmiddelen-l.svg')">
+      <div class="d-inline-block">
+        <div class="d-inline d-xl-none">
+          Ontsluiten en hulpmiddelen
+        </div>
+        <div class="status-meter-icon-wrapper">
+          @foreach (array(1, 2, 3, 4, 5) as $num)
+            @if (get_field('project_status_ontsluiten_en_hulpmiddelen_score') == $num)
+              <div class="status-meter-icon status-score-{{ $num }} mx-auto"></div>
+            @else
+              <div class="status-meter-icon mx-auto"></div>
+            @endif
+          @endforeach
+        </div>
       </div>
     </div>
   </li>
@@ -41,9 +74,20 @@
       <div class="d-none d-xl-block">
         Duurzame ontsluiting
       </div>
-      <img class="d-inline d-xl-block img-fluid" src="@asset('images/OSF-Icon-ontsluiting-l.svg')">
-      <div class="d-inline d-xl-none">
-        Duurzame ontsluiting
+      <img class="align-top d-inline d-xl-block img-fluid" src="@asset('images/OSF-Icon-ontsluiting-l.svg')">
+      <div class="d-inline-block">
+        <div class="d-inline d-xl-none">
+          Duurzame ontsluiting
+        </div>
+        <div class="status-meter-icon-wrapper">
+          @foreach (array(1, 2, 3, 4, 5) as $num)
+            @if (get_field('project_status_duurzame_ontsluiting_score') == $num)
+              <div class="status-meter-icon status-score-{{ $num }} mx-auto"></div>
+            @else
+              <div class="status-meter-icon mx-auto"></div>
+            @endif
+          @endforeach
+        </div>
       </div>
     </div>
   </li>
@@ -54,9 +98,20 @@
         <br>
         <br>
       </div>
-      <img class="d-inline d-xl-block img-fluid" src="@asset('images/OSF-Icon-hergebruik-l.svg')">
-      <div class="d-inline d-xl-none">
-        Hergebruik
+      <img class="align-top d-inline d-xl-block img-fluid" src="@asset('images/OSF-Icon-hergebruik-l.svg')">
+      <div class="d-inline-block">
+        <div class="d-inline d-xl-none">
+          Hergebruik
+        </div>
+        <div class="status-meter-icon-wrapper">
+          @foreach (array(1, 2, 3, 4, 5) as $num)
+            @if (get_field('project_status_hergebruik_score') == $num)
+              <div class="status-meter-icon status-score-{{ $num }} mx-auto"></div>
+            @else
+              <div class="status-meter-icon mx-auto"></div>
+            @endif
+          @endforeach
+        </div>
       </div>
     </div>
   </li>
