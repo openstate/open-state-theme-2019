2c4e9934fdc1a631675c5f20c7ec879e6301f6d6
Sicco van Sas <siccovansas@users.noreply.github.com>
2019-08-02 19:45:54 +0200

    Translate some strings

--- resources/views/partials/status-carousel-controls.blade.php
+++ resources/views/partials/status-carousel-controls.blade.php
@@ -2,14 +2,26 @@
   <li class="col-12 col-xl-2 offset-1 active" data-target="#carouselStatusControls" data-slide-to="0">
     <div class="status-indicator text-xl-center">
       <div class="d-none d-xl-block">
-        Lobby
+        <? _e("
+          <!--:nl-->
+            Lobby
+          <!--:--><!--:en-->
+            Lobby
+          <!--:-->
+        ") ?>
         <br>
         <br>
       </div>
       <img class="align-top d-inline d-xl-block img-fluid" src="@asset('images/OSF-Icon-lobby-l.svg')">
       <div class="d-inline-block">
         <div class="d-inline d-xl-none">
-          Lobby
+          <? _e("
+            <!--:nl-->
+              Lobby
+            <!--:--><!--:en-->
+              Lobby
+            <!--:-->
+          ") ?>
         </div>
         <div class="status-meter-icon-wrapper">
           @foreach (range(1, 5) as $num)
@@ -26,14 +38,26 @@
   <li class="col-12 col-xl-2" data-target="#carouselStatusControls" data-slide-to="1">
     <div class="status-indicator text-xl-center">
       <div class="d-none d-xl-block">
-        Data beschikbaar
+        <? _e("
+          <!--:nl-->
+            Data beschikbaar
+          <!--:--><!--:en-->
+            Data available
+          <!--:-->
+        ") ?>
         <br>
         <br>
       </div>
       <img class="align-top d-inline d-xl-block img-fluid" src="@asset('images/OSF-Icon-data-beschikbaar-l.svg')">
       <div class="d-inline-block">
         <div class="d-inline d-xl-none">
-          Data beschikbaar
+          <? _e("
+            <!--:nl-->
+              Data beschikbaar
+            <!--:--><!--:en-->
+              Data available
+            <!--:-->
+          ") ?>
         </div>
         <div class="status-meter-icon-wrapper">
           @foreach (array(1, 2, 3, 4, 5) as $num)
@@ -50,12 +74,24 @@
   <li class="col-12 col-xl-2" data-target="#carouselStatusControls" data-slide-to="2">
     <div class="status-indicator text-xl-center">
       <div class="d-none d-xl-block">
-        Ontsluiten en hulpmiddelen
+        <? _e("
+          <!--:nl-->
+            Ontsluiten en hulpmiddelen
+          <!--:--><!--:en-->
+            Release and Tools
+          <!--:-->
+        ") ?>
       </div>
       <img class="align-top d-inline d-xl-block img-fluid" src="@asset('images/OSF-Icon-hulpmiddelen-l.svg')">
       <div class="d-inline-block">
         <div class="d-inline d-xl-none">
-          Ontsluiten en hulpmiddelen
+          <? _e("
+            <!--:nl-->
+              Ontsluiten en hulpmiddelen
+            <!--:--><!--:en-->
+              Release and tools
+            <!--:-->
+          ") ?>
         </div>
         <div class="status-meter-icon-wrapper">
           @foreach (array(1, 2, 3, 4, 5) as $num)
@@ -72,12 +108,24 @@
   <li class="col-12 col-xl-2" data-target="#carouselStatusControls" data-slide-to="3">
     <div class="status-indicator text-xl-center">
       <div class="d-none d-xl-block">
-        Duurzame ontsluiting
+        <? _e("
+          <!--:nl-->
+            Duurzame ontsluiting
+          <!--:--><!--:en-->
+            Durable releasing
+          <!--:-->
+        ") ?>
       </div>
       <img class="align-top d-inline d-xl-block img-fluid" src="@asset('images/OSF-Icon-ontsluiting-l.svg')">
       <div class="d-inline-block">
         <div class="d-inline d-xl-none">
-          Duurzame ontsluiting
+          <? _e("
+            <!--:nl-->
+              Duurzame ontsluiting
+            <!--:--><!--:en-->
+              Durable releasing
+            <!--:-->
+          ") ?>
         </div>
         <div class="status-meter-icon-wrapper">
           @foreach (array(1, 2, 3, 4, 5) as $num)
@@ -94,14 +142,26 @@
   <li class="col-12 col-xl-2" data-target="#carouselStatusControls" data-slide-to="4">
     <div class="status-indicator text-xl-center">
       <div class="d-none d-xl-block">
-        Hergebruik
+        <? _e("
+          <!--:nl-->
+            Hergebruik
+          <!--:--><!--:en-->
+            Reuse
+          <!--:-->
+        ") ?>
         <br>
         <br>
       </div>
       <img class="align-top d-inline d-xl-block img-fluid" src="@asset('images/OSF-Icon-hergebruik-l.svg')">
       <div class="d-inline-block">
         <div class="d-inline d-xl-none">
-          Hergebruik
+          <? _e("
+            <!--:nl-->
+              Hergebruik
+            <!--:--><!--:en-->
+              Reuse
+            <!--:-->
+          ") ?>
         </div>
         <div class="status-meter-icon-wrapper">
           @foreach (array(1, 2, 3, 4, 5) as $num)
