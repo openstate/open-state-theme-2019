7b30004cce13c6bbc2581a84e7ff00391672fd38
Sicco van Sas <siccovansas@users.noreply.github.com>
2019-08-15 16:12:31 +0200

    Add div otherwise the mailchimp script wont work

--- resources/views/partials/newsletter.blade.php
+++ resources/views/partials/newsletter.blade.php
@@ -1,18 +1,19 @@
-<form action="https://openstate.us4.list-manage.com/subscribe/post?u=03355fd4f1a7935cae63b21aa&amp;id=a9619e4f3e" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
-  <label for="mce-EMAIL" class="footer-form">
-    <? _e("
-      <!--:nl-->
-        <span class='font-weight-bold'>OP DE HOOGTE BLIJVEN?</span> MELD JE AAN VOOR DE MAANDELIJKSE NIEUWSBRIEF:
-      <!--:--><!--:en-->
-        <span class='font-weight-bold'>STAY UP TO DATE?</span> SUBSCRIBE TO OUR MONTHLY NEWSLETTER:
-      <!--:-->
-    ") ?>
-  </label>
-  <div id="mc_embed_signup_scroll">
-    <div class="row">
-      <div class="col-12">
-        <div class="mc-field-group mc-first-group">
-          <input type="email" placeholder="
+<div>
+  <form action="https://openstate.us4.list-manage.com/subscribe/post?u=03355fd4f1a7935cae63b21aa&amp;id=a9619e4f3e" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
+    <label for="mce-EMAIL" class="footer-form">
+      <? _e("
+        <!--:nl-->
+          <span class='font-weight-bold'>OP DE HOOGTE BLIJVEN?</span> MELD JE AAN VOOR DE MAANDELIJKSE NIEUWSBRIEF:
+        <!--:--><!--:en-->
+          <span class='font-weight-bold'>STAY UP TO DATE?</span> SUBSCRIBE TO OUR MONTHLY NEWSLETTER:
+        <!--:-->
+      ") ?>
+    </label>
+    <div id="mc_embed_signup_scroll">
+      <div class="row">
+        <div class="col-12">
+          <div class="mc-field-group mc-first-group">
+            <input type="email" placeholder="
 <? _e("
 <!--:nl-->
 e-mailadres
@@ -20,39 +21,40 @@ e-mailadres
 email address
 <!--:-->
 ") ?>
-          " value="" name="EMAIL" class="email form-control" id="mce-EMAIL">
+            " value="" name="EMAIL" class="email form-control" id="mce-EMAIL">
+          </div>
         </div>
       </div>
+      <div id="mce-responses" class="clear">
+        <div class="response" id="mce-error-response" style="display:none"></div>
+        <div class="response" id="mce-success-response" style="display:none; color:green"></div>
+      </div>
+      <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
+      <div style="position: absolute; left: -5000px;" aria-hidden="true">
+        <input type="text" name="b_03355fd4f1a7935cae63b21aa_a9619e4f3e" tabindex="-1" value="">
+        <input type="submit" name="subscribe" id="mc-embedded-subscribe style="position: absolute; left: -9999px; width: 1px; height: 1px;" tabindex="-1"">
+      </div>
     </div>
-    <div id="mce-responses" class="clear">
-      <div class="response" id="mce-error-response" style="display:none"></div>
-      <div class="response" id="mce-success-response" style="display:none; color:green"></div>
-    </div>
-    <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
-    <div style="position: absolute; left: -5000px;" aria-hidden="true">
-      <input type="text" name="b_03355fd4f1a7935cae63b21aa_a9619e4f3e" tabindex="-1" value="">
-      <input type="submit" name="subscribe" id="mc-embedded-subscribe style="position: absolute; left: -9999px; width: 1px; height: 1px;" tabindex="-1"">
-    </div>
-  </div>
-</form>
-<p>
-  <? _e("
-    <!--:nl-->
-    Wat kan je verwachten?
-    <!--:--><!--:en-->
-    What can you expect?
-    <!--:-->
-  ") ?>
-  <a href="https://us4.campaign-archive.com/home/?u=03355fd4f1a7935cae63b21aa&id=a9619e4f3e" target="_blank" rel="noopener">
+  </form>
+  <p>
     <? _e("
       <!--:nl-->
-      Zie onze eerdere nieuwsbrieven.
+      Wat kan je verwachten?
       <!--:--><!--:en-->
-      See our previous newsletters.
+      What can you expect?
       <!--:-->
     ") ?>
-  </a>
-</p>
+    <a href="https://us4.campaign-archive.com/home/?u=03355fd4f1a7935cae63b21aa&id=a9619e4f3e" target="_blank" rel="noopener">
+      <? _e("
+        <!--:nl-->
+        Zie onze eerdere nieuwsbrieven.
+        <!--:--><!--:en-->
+        See our previous newsletters.
+        <!--:-->
+      ") ?>
+    </a>
+  </p>
+</div>
 <script type='text/javascript' src='//s3.amazonaws.com/downloads.mailchimp.com/js/mc-validate.js'></script>
 <script type='text/javascript'>
   var $mcj = jQuery.noConflict(true);
