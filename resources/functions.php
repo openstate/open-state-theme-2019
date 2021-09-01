a7e2c99ea4ef1c0eca27c3fc469d3053a3256f52
Sicco van Sas <siccovansas@users.noreply.github.com>
2021-09-01 16:57:32 +0200

    Add SVG to allowed mime types

--- resources/functions.php
+++ resources/functions.php
@@ -137,13 +137,14 @@ function set_posts_per_page($query) {
 }
 add_action('pre_get_posts', 'set_posts_per_page');
 
-// Allow videos to be uploaded
+// Allow videos and SVG to be uploaded
 add_filter('mime_types', 'extend_mime_types');
 function extend_mime_types( $existing_mimes=array() ) {
-    // Add webm, mp4 and OGG to the list of mime types
+    // Add webm, mp4, OGG and SVG to the list of mime types
     $existing_mimes['webm'] = 'video/webm';
     $existing_mimes['mp4']  = 'video/mp4';
     $existing_mimes['ogg']  = 'video/ogg';
+    $existing_mimes['svg']  = 'image/svg+xml';
 
     // Return an array now including our added mime types
     return $existing_mimes;
